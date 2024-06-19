9936d6318cf4fc6ba6dd1ccf66ab63e9dee18b67
Khuong <minhkhuong782k3@gmail.com>
2024-06-19 11:32:52 +0700

    Tạo trang chi tiết sản phẩm bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,7 +1,7 @@
 const Product = require('./../../models/product.model');
 
 // [GET] /products
-const index = async (req, res)=> {
+module.exports.index = async (req, res)=> {
     const products = await Product.find({
         status: 'active',
         deleted: false
@@ -9,4 +9,21 @@ const index = async (req, res)=> {
     res.render('./client/pages/products/index.pug', {title: 'Danh sách sản phẩm', products});
 }
 
-module.exports = {index};
+
+module.exports.detail = async (req, res) => {
+    const slug = req.params.slug;
+    try {
+        const product = await Product.findOne({
+            deleted: false,
+            slug: slug
+        });
+
+        res.render('./client/pages/products/detail.pug', {
+            title: product.title,
+            product: product
+        })
+    }
+    catch(err){
+        res.redirect('back');
+    }
+}
