910dd75a0a37b553c1f4a356a519085a9c7806b3
Khuong <minhkhuong782k3@gmail.com>
2024-06-23 01:06:38 +0700

    update

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -34,7 +34,7 @@ module.exports.index = async (req, res) => {
         totalProduct, 
         {
             currentPage: 1,
-            limitItems: 5,   // số sp trong 1 trang sản phẩm khi phân trang
+            limitItems: 10,   // số sp trong 1 trang sản phẩm khi phân trang
             totalPage: 1
         }
     )
