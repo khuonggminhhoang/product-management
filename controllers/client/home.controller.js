5b97ff31a8a46bea7f056cde1f2124e3cca4f527
Khuong <minhkhuong782k3@gmail.com>
2024-06-30 23:27:01 +0700

    [client] Hiển thị sản phẩm mới

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -7,11 +7,17 @@ const index = async (req, res) => {
             featured: true,
             status: 'active',
             deleted: false
-        }).limit(10); 
+        }).limit(6); 
+
+        const newProducts = await Product.find({
+            status: 'active',
+            deleted: false
+        }).sort({position: 'desc'}).limit(12);
 
         res.render('./client/pages/home/index.pug', {
             title: 'Trang chủ',
-            featuredProducts: featuredProducts
+            featuredProducts: featuredProducts,
+            newProducts: newProducts
         });
     }
     catch(err) {
