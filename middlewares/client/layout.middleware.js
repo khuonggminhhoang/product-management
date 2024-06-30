a0805583e9d054e3f562ed8c65bcd6588546d526
Khuong <minhkhuong782k3@gmail.com>
2024-06-30 10:49:08 +0700

    [client] Thêm layout blog

--- middlewares/client/layout.middleware.js
+++ middlewares/client/layout.middleware.js
@@ -1,4 +1,5 @@
 const ProductCategory = require('./../../models/product-category.model');
+const ArticleCategory = require('./../../models/article-category.model');
 
 const createTree = require('./../../helpers/createTree');
 
@@ -6,5 +7,12 @@ module.exports.productCategory = async (req, res, next) => {
     const arr = await ProductCategory.find({status: 'active', deleted: false});
     res.locals.layoutProductCategory = createTree(arr, '');
     
+    next();
+}
+
+module.exports.articleCategory = async (req, res, next) => {
+    const arr = await ArticleCategory.find({status: 'active', deleted: false});
+    res.locals.layoutArticleCategory = createTree(arr, '');
+
     next();
 }
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,10 +1,11 @@
 const homeRoutes = require('./home.route.js');
 const productRoutes = require('./product.route.js');
 
-const middleware = require('./../../middlewares/client/product-category.middleware.js')
+const middleware = require('./../../middlewares/client/layout.middleware.js')
 
 module.exports = (app) => {
     app.use(middleware.productCategory);
+    app.use(middleware.articleCategory);
 
     app.use("/", homeRoutes);
     app.use('/products', productRoutes);
