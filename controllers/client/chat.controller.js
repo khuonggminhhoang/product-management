8452c3724828f7d22da60dbfc68558a7d97e0cf3
Khuong <minhkhuong782k3@gmail.com>
2024-07-23 00:47:09 +0700

    [admin] Tạo tính năng chat realtime global

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,16 +1,46 @@
+const Chat = require('./../../models/chat.model');
+const User = require('./../../models/user.model');
+
 // [GET] /chat
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+    const userId = res.locals.user.id;
+    const fullName = res.locals.user.fullName;
+
     // SOCKET IO
-    global._io.once('connection', (socket) => {
-        socket.on('CLIENT_SEND_MESSAGES', (msg) => {
+    _io.once('connection', (socket) => {
+        socket.on('CLIENT_SEND_MESSAGES', async (content) => {
             // Gửi lại tới client
-            socket.emit('SERVER_RETURN_MESSAGES', `${socket.id}: ${msg}`);
-    
-        })
+            const chat = new Chat({
+                userId: userId,
+                content: content
+            });
+            await chat.save();
+
+            _io.emit('SERVER_RETURN_MESSAGES', {
+                avatar: res.locals.user.avatar,
+                userId: userId,
+                fullName: fullName,
+                content: content
+            });
+        });
+
     });
+
+    const chats = await Chat.find({
+        deleted: false
+    });
+     
+    for(let chat of chats) {
+        const userInfo = await User.findOne({
+            _id: chat.userId
+        }).select('fullName avatar');
+
+        chat.userInfo = userInfo;
+    }
     
     // END
     res.render('./client/pages/chat/index.pug', {
-        title: 'Chat'
+        title: 'Chat',
+        chats: chats
     });
 }
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,13 +1,57 @@
-const form = document.querySelector('.inner-form');
+// Xử lý sự kiện gửi tin nhắn
+const form = document.querySelector('.chat .inner-form');
 if(form) {
-    const input = form.querySelector('input[name="message"]');
+    const input = form.querySelector('input[name="content"]');
     form.addEventListener('submit', (e) => {
         e.preventDefault();
         socket.emit('CLIENT_SEND_MESSAGES', input.value);
         input.value = "";
     });
 
-    socket.on('SERVER_RETURN_MESSAGES', (msg) => {
-        console.log(msg);
+    socket.on('SERVER_RETURN_MESSAGES', (infoMessage) => {
+        console.log(infoMessage);
+
     });
 }
+// End
+
+// Xử lý sự kiện thêm tin nhắn realtime
+const chat = document.querySelector('.chat');
+if(chat) {
+    const innerBody = chat.querySelector('.inner-body');
+    const myId = chat.getAttribute('my-id');
+    
+    socket.on('SERVER_RETURN_MESSAGES', (infoMessage) => {
+        const div = document.createElement('div');
+
+        let innerAvatar = '';
+        let innerName = '';
+        if(myId != infoMessage.userId) {
+            div.className = 'inner-incoming';
+            innerAvatar = `
+                <div class='inner-avatar'>
+                    <img src=${infoMessage.avatar} />    
+                </div>
+            `;
+
+            innerName = `
+                <small class='inner-name'>${infoMessage.fullName}</small>
+            `;
+        }     
+        else {
+            div.className = 'inner-outgoing';
+        }
+        
+        div.innerHTML = `
+            ${innerAvatar}
+            <div class='inner-text'>
+                ${innerName}
+                <div class='inner-content'>${infoMessage.content}</div>
+            </div>
+        `;
+        
+        innerBody.appendChild(div);
+    });
+}
+// End
+
