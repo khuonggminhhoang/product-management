16bcc744438c6d49191a40b38eb3f51ceb3ee80a
Khuong <minhkhuong782k3@gmail.com>
2024-07-29 17:07:43 +0700

    update

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -5,52 +5,9 @@ const chatSocket = require('./../../sockets/client/chat.socket');
 
 // [GET] /chat
 module.exports.index = async (req, res) => {
-    
-
     // SOCKET IO
     chatSocket(res);
-    // _io.once('connection', (socket) => {
-    //     socket.on('CLIENT_SEND_MESSAGES', async (blockInfo) => {
-    //         // Gửi lại tới client
-    //         const images = [];
-    //         for(let imageBuffer of blockInfo.images) {
-    //             const linkImage = await uploadToCloudinaryHelper.uploadImages(imageBuffer);
-    //             images.push(linkImage);
-    //         }
-
-    //         const chat = new Chat({
-    //             userId: userId,
-    //             content: blockInfo.content,
-    //             images: images
-    //         });
-    //         await chat.save();
-
-    //         _io.emit('SERVER_RETURN_MESSAGES', {
-    //             avatar: res.locals.user.avatar,
-    //             userId: userId,
-    //             fullName: fullName,
-    //             content: blockInfo.content,
-    //             images: images
-    //         }); 
-    //     });
-
-    //     let timer;          // set time cho dấu nháy sau 3 giây mới ngắt
-    //     socket.on('CLIENT_SEND_TYPING', (state) => {
-    //         socket.broadcast.emit('SERVER_RETURN_TYPING', {
-    //             fullName: fullName,
-    //             state: state
-    //         });
-            
-    //         if(state == 'show') {
-    //             clearTimeout(timer);
-
-    //             timer = setTimeout(() => {
-    //                 socket.broadcast.emit('SERVER_RETURN_TYPING', 'hidden');
-    //             }, 3000);
-    //         }
-    //     });
-
-    // });
+    // End Socket io
 
     const chats = await Chat.find({
         deleted: false
